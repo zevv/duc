c1c2f938100977cbafe0f96d2fbc7d407b96555e
Ico Doornekamp <ico@pruts.nl>
2015-04-12 12:47:37 +0200

    Reworked duc gui to reduce indenting in mainloop.

--- duc/gui.c
+++ duc/gui.c
@@ -19,14 +19,138 @@
 #include <X11/keysym.h>
 
 static int depth = 4;
+static int redraw = 1;
+static int tooltip_x = 0;
+static int tooltip_y = 0;
+static struct pollfd pfd;
+static int palette = 0;
+static int win_w = 600;
+static int win_h = 600;
+static double fuzz = 0.3;
+static cairo_surface_t *cs;
+static duc_dir *dir;
+static duc_graph *graph;
+
+static void draw(void)
+{
+	cairo_t *cr;
+
+	cairo_surface_t *cs2 = cairo_surface_create_similar(
+			cs, 
+			CAIRO_CONTENT_COLOR,
+			cairo_xlib_surface_get_width(cs),
+			cairo_xlib_surface_get_height(cs));
+
+	cr = cairo_create(cs2);
+
+	cairo_set_source_rgb(cr, 1, 1, 1);
+	cairo_paint(cr);
+
+	int size = win_w < win_h ? win_w : win_h;
+	int pos_x = (win_w - size) / 2;
+	int pos_y = (win_h - size) / 2;
+
+	if(depth < 2) depth = 2;
+	if(depth > 10) depth = 10;
+
+	duc_graph_set_size(graph, size);
+	duc_graph_set_position(graph, pos_x, pos_y);
+	duc_graph_set_max_level(graph, depth);
+	duc_graph_set_fuzz(graph, fuzz);
+	duc_graph_set_max_name_len(graph, 30);
+	duc_graph_draw_cairo(graph, dir, cr);
+	cairo_destroy(cr);
+
+	cr = cairo_create(cs);
+	cairo_set_source_surface(cr, cs2, 0, 0);
+	cairo_paint(cr);
+	cairo_destroy(cr);
+
+	cairo_surface_destroy(cs2);
+}
+
+
+static void handle_event(XEvent e)
+{
+	KeySym k;
+	int x, y, b;
+
+	switch(e.type) {
+
+		case ConfigureNotify: 
+			win_w = e.xconfigure.width;
+			win_h = e.xconfigure.height;
+			cairo_xlib_surface_set_size(cs, win_w, win_h);
+			redraw = 1;
+			break;
+			
+
+		case Expose:
+			if(e.xexpose.count < 1) redraw = 1;
+			break;
+
+		case KeyPress: 
+
+			k = XLookupKeysym(&e.xkey, 0);
+
+			if(k == XK_minus) depth--;
+			if(k == XK_equal) depth++;
+			if(k == XK_0) depth = 4;
+			if(k == XK_Escape) exit(0);
+			if(k == XK_q) exit(0);
+			if(k == XK_f) fuzz = 0.7 - fuzz;
+			if(k == XK_p) {
+				palette = (palette + 1) % 4;
+				duc_graph_set_palette(graph, palette);
+			}
+			if(k == XK_BackSpace) {
+				duc_dir *dir2 = duc_dir_openat(dir, "..");
+				if(dir2) {
+					duc_dir_close(dir);
+					dir = dir2;
+				}
+			}
+
+			redraw = 1;
+			break;
+
+		case ButtonPress: 
+
+			x = e.xbutton.x;
+			y = e.xbutton.y;
+			b = e.xbutton.button;
+
+			if(b == 1) {
+				duc_dir *dir2 = duc_graph_find_spot(graph, dir, x, y);
+				if(dir2) {
+					duc_dir_close(dir);
+					dir = dir2;
+				}
+			}
+			if(b == 3) {
+				duc_dir *dir2 = duc_dir_openat(dir, "..");
+				if(dir2) {
+					duc_dir_close(dir);
+					dir = dir2;
+				}
+			}
+
+			if(b == 4) depth --;
+			if(b == 5) depth ++;
+
+			redraw = 1;
+			break;
+
+		case MotionNotify: 
+
+			tooltip_x = e.xmotion.x;
+			tooltip_y = e.xmotion.y;
+	}
+}
 
 
 int do_gui(duc *duc, duc_graph *graph, duc_dir *dir)
 {
-	int palette = 0;
-	int win_w = 600;
-	int win_h = 600;
-	double fuzz = 0.3;
 
 	Display *dpy = XOpenDisplay(NULL);
 	if(dpy == NULL) {
@@ -47,54 +171,16 @@ int do_gui(duc *duc, duc_graph *graph, duc_dir *dir)
 	XSelectInput(dpy, win, ExposureMask | ButtonPressMask | StructureNotifyMask | KeyPressMask | PointerMotionMask);
 	XMapWindow(dpy, win);
 	
-	cairo_surface_t *cs = cairo_xlib_surface_create(dpy, win, DefaultVisual(dpy, 0), win_w, win_h);
+	cs = cairo_xlib_surface_create(dpy, win, DefaultVisual(dpy, 0), win_w, win_h);
 
-	int redraw = 1;
-	int tooltip_x = 0;
-	int tooltip_y = 0;
-	struct pollfd pfd;
 
 	pfd.fd = ConnectionNumber(dpy);
 	pfd.events = POLLIN | POLLERR;
 
 	while(1) {
-	
-		if(redraw && cs) {
-			cairo_t *cr;
 
-			cairo_surface_t *cs2 = cairo_surface_create_similar(
-					cs, 
-					CAIRO_CONTENT_COLOR,
-					cairo_xlib_surface_get_width(cs),
-					cairo_xlib_surface_get_height(cs));
-
-			cr = cairo_create(cs2);
-
-			cairo_set_source_rgb(cr, 1, 1, 1);
-			cairo_paint(cr);
-
-			int size = win_w < win_h ? win_w : win_h;
-			int pos_x = (win_w - size) / 2;
-			int pos_y = (win_h - size) / 2;
-
-			if(depth < 2) depth = 2;
-			if(depth > 10) depth = 10;
-
-			duc_graph_set_size(graph, size);
-			duc_graph_set_position(graph, pos_x, pos_y);
-			duc_graph_set_max_level(graph, depth);
-			duc_graph_set_fuzz(graph, fuzz);
-			duc_graph_set_max_name_len(graph, 30);
-			duc_graph_draw_cairo(graph, dir, cr);
-			cairo_destroy(cr);
-
-			cr = cairo_create(cs);
-			cairo_set_source_surface(cr, cs2, 0, 0);
-			cairo_paint(cr);
-			cairo_destroy(cr);
-
-			cairo_surface_destroy(cs2);
-
+		if(redraw) {
+			draw();
 			XFlush(dpy);
 			redraw = 0;
 		}
@@ -106,86 +192,12 @@ int do_gui(duc *duc, duc_graph *graph, duc_dir *dir)
 			redraw = 1;
 		}
 
-
-		XEvent e;
-
 		while (XEventsQueued(dpy, QueuedAfterReading) > 0) {
+			XEvent e;
 			XNextEvent(dpy, &e);
 
-			switch(e.type) {
+			handle_event(e);
 
-				case ConfigureNotify: {
-					win_w = e.xconfigure.width;
-					win_h = e.xconfigure.height;
-					cairo_xlib_surface_set_size(cs, win_w, win_h);
-					redraw = 1;
-					break;
-				}
-
-				case Expose:
-					if(e.xexpose.count < 1) redraw = 1;
-					break;
-				
-				case KeyPress: {
-
-					KeySym k = XLookupKeysym(&e.xkey, 0);
-
-					if(k == XK_minus) depth--;
-					if(k == XK_equal) depth++;
-					if(k == XK_0) depth = 4;
-					if(k == XK_Escape) exit(0);
-					if(k == XK_q) exit(0);
-					if(k == XK_f) fuzz = 0.7 - fuzz;
-					if(k == XK_p) {
-						palette = (palette + 1) % 4;
-						duc_graph_set_palette(graph, palette);
-					}
-					if(k == XK_BackSpace) {
-						duc_dir *dir2 = duc_dir_openat(dir, "..");
-						if(dir2) {
-							duc_dir_close(dir);
-							dir = dir2;
-						}
-					}
-
-					redraw = 1;
-					break;
-				}
-
-				case ButtonPress: {
-
-					int x = e.xbutton.x;
-					int y = e.xbutton.y;
-					int b = e.xbutton.button;
-
-					if(b == 1) {
-						duc_dir *dir2 = duc_graph_find_spot(graph, dir, x, y);
-						if(dir2) {
-							duc_dir_close(dir);
-							dir = dir2;
-						}
-					}
-					if(b == 3) {
-						duc_dir *dir2 = duc_dir_openat(dir, "..");
-						if(dir2) {
-							duc_dir_close(dir);
-							dir = dir2;
-						}
-					}
-
-					if(b == 4) depth --;
-					if(b == 5) depth ++;
-
-					redraw = 1;
-					break;
-				}
-
-				case MotionNotify: {
-
-					tooltip_x = e.xmotion.x;
-					tooltip_y = e.xmotion.y;
-				}
-			}
 		}
 	}
 
@@ -245,13 +257,13 @@ int gui_main(int argc, char *argv[])
 		return -1;
 	}
 	
-	duc_dir *dir = duc_dir_open(duc, path);
+	dir = duc_dir_open(duc, path);
 	if(dir == NULL) {
 		duc_log(duc, DUC_LOG_WRN, "%s", duc_strerror(duc));
 		return -1;
 	}
 
-	duc_graph *graph = duc_graph_new(duc);
+	graph = duc_graph_new(duc);
 
 	do_gui(duc, graph, dir);
 
