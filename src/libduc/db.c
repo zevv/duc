af23d62c7a72dc92609c1759551f3fa86e3ff8f1
Ico Doornekamp <ico@pruts.nl>
2015-04-29 09:04:17 +0200

    Removed warnings fro db.c if key not found

--- src/libduc/db.c
+++ src/libduc/db.c
@@ -73,7 +73,6 @@ struct duc_dir *db_read_dir(struct duc *duc, struct duc_devino *devino)
 	keyl = mkkey(devino, key, sizeof key);
 	char *val = db_get(duc->db, key, keyl, &vall);
 	if(val == NULL) {
-		duc_log(duc, DUC_LOG_WRN, "Key %s not found in database", key);
 		duc->err = DUC_E_PATH_NOT_FOUND;
 		return NULL;
 	}
